5d95a629352846591ff6deb6ad9f17e7dc142604
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2024-06-23 23:11:00 -0300

    fix serial_port.py to send tank_id

--- serial_port_reader.py
+++ serial_port_reader.py
@@ -7,15 +7,19 @@ arduino = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     if arduino.in_waiting > 0:
         # Read data from serial port
-        data = arduino.readline().decode().strip()
-        print(data)
+        tank_id, level = arduino.readline().decode().strip().split("|")
+        print(tank_id, level)
 
         try:
-            level = float(data)
+            tank_id = int(tank_id)
+            level = float(level)
 
             # Send data to server
             print("Sending data to server")
-            payload = {"level": level}
+            payload = {
+                "tank_id": tank_id,
+                "level": level
+            }
             response = requests.post(url, data=payload)
 
             # Print response from server
